deaee1d6589912c8f1ab1b05af3a631050e148f5
VEIIEV <62066130+VEIIEV@users.noreply.github.com>
2020-03-19 22:38:36 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,43 +11,42 @@ int main()
 {
 	setlocale(LC_ALL, "Russian");
 	
-	char* a;
-	a= new char[5] { '?', 'z', '?', '?' };
+	char a[5]{ '?', 'z', '?', '?' };
 	List();
 	Software firstl; //поумолчанию
 	Software secondl = Software(a, a, a); //с переменными
 	Software trel(a, a, a); // копирование
 	int toggle=1;
-	while (true)
+	int z=1; while (z==1)
 	{
 		cin >> toggle;
 		switch (toggle)
 		{
-		case 1:	firstl.set(a, a, a, 5); List(); break;
+		case 1:	firstl.set(a, a, a); List(); break;
 		case 2: secondl.get(firstl); break;
 		case 3: firstl.Printsoft(); break;
 		case 4: secondl.Printsoft(); break;
 		case 5: trel.Printsoft(); break;
-		case 6: delete[]a;
-			a = 0;
-			return 0;
+		case 6:
+			z = 0;
+			break;
 		default: List();
 		}
 	}
-	
-	Math first;
-	Math second;
+	//////////////////////////////////////////
+	Software first;
+	Software second;
 	if (first > second) { cout << "yes" << endl; }
 	else { cout << "no" << endl; }
-	cin >> first;
-	cout << first;
+	cin >> first; //ввод в класс
+	cout << first; //вывод из класса
 	first = second; //оператор присваивания переопределён изначально, для любого класса
-	cout << first;
-	firstl.Printsoft(); //операция присваивания для массива данных типа char*
+	cout << first; 
+	firstl.Printsoft();
 	secondl.Printsoft();
-	secondl = firstl;
+	secondl = firstl;//операция присваивания для массива данных типа char*
 	secondl.Printsoft();
-	delete[]a;
 	return 0;
 }
+
 
